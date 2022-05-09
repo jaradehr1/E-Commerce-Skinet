ee8b9fc1b20ba945dd70eca977b27bb219e2d357
Riad <riad.jaradeh@meadowsbh.com>
2022-05-09 10:03:59 -0700

    Adding Error Handling

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -5,14 +5,13 @@ using Microsoft.AspNetCore.Mvc;
 using Core.Interfaces;
 using Core.Specifications;
 using API.Dtos;
-using System.Linq;
 using AutoMapper;
+using API.Errors;
+using Microsoft.AspNetCore.Http;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class ProductsController : ControllerBase
+    public class ProductsController : BaseApiController
     {
         private readonly IGenericRepository<ProductType> _productTypeRepo;
         private readonly IMapper _mapper;
@@ -40,10 +39,15 @@ namespace API.Controllers
         }
 
         [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
         public async Task<ActionResult<ProductToReturnDto>> GetProduct(int id) 
         {
             var spec = new ProductsWithTypesAndBrandsSpecification(id);
             var product = await _productRepo.GetEntityWIthSpec(spec);
+
+            if (product == null) return NotFound(new ApiResponse(404));
+
             var productToReturn = _mapper.Map<Product, ProductToReturnDto>(product);
             return Ok(productToReturn);
         }
